062e47002d81b1beeb5773b102529201d6e4c276
Kircérta <alexxontoronto@icloud.com>
2026-01-30 04:22:30 -0500

    Initial Commit

---
+++ ImageSizeTransformer/ImageSizeTransformerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSizeTransformerApp.swift
+//  ImageSizeTransformer
+//
+//  Created by Kircérta on 2026-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ImageSizeTransformerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
